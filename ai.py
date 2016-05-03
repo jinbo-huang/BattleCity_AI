45b8303367ea93d34bf755aaabcd5918f6fea426
Jin-Bo Huang <iceman1216i@gmail.com>
2016-05-03 22:04:48 +0800

    1. remove the slow part. 2. add encode map part.

--- ai.py
+++ ai.py
@@ -1,11 +1,19 @@
 import random
 import time
 import multiprocessing
+import sys
+
 class ai_agent():
 	mapinfo = []
+	encoded_map = []
+	#map_width = 0
+	#map_height = 0
+
 	def __init__(self):
 		self.mapinfo = []
-
+		self.encoded_map = []
+		self.map_width = 13
+		self.map_height = 13
 	# rect:					[left, top, width, height]
 	# rect_type:			0:empty 1:brick 2:steel 3:water 4:grass 5:froze
 	# castle_rect:			[12*16, 24*16, 32, 32]
@@ -29,12 +37,27 @@ class ai_agent():
 		while True:
 		#-----your ai operation,This code is a random strategy,please design your ai !!-----------------------			
 			self.Get_mapInfo(p_mapinfo)
-			print self.mapinfo[0]
-			#time.sleep(0.001)	
+
+			bullets = self.mapinfo[0]
+			enemies = self.mapinfo[1]
+			tiles = self.mapinfo[2]
+			player = self.mapinfo[3][0]
+			player_left = player[0][0]
+			player_top = player[0][1]
+
+			self.encode_map(bullets, enemies, tiles, player)
+
+			self.print_encoded_map()
+			# raw_input()
+			#print "player left:%s" %(player_left)
+			#print "player top:%s" %(player_top)
+
+			# print self.mapinfo[0]
+			# time.sleep(0.001)	
 			
-			q=0
-			for i in range(10000000):
-				q+=1
+			#q=0
+			#for i in range(10000000):
+			#	q+=1
 			
 			shoot = random.randint(0,1)
 			move_dir = random.randint(0,4)
@@ -43,6 +66,40 @@ class ai_agent():
 			#-----------
 			self.Update_Strategy(c_control,shoot,move_dir,keep_action)
 		#------------------------------------------------------------------------------------------------------
+
+
+	def encode_map(self, bullets, enemies, tiles, player):
+		result = [['_' for x in range(self.map_width)] for y in range(self.map_height)]
+
+		for bullet in bullets:
+			b_left = bullet[0][0]
+			b_top = bullet[0][1]
+			result[b_top / 32][b_left / 32] = 'B'
+
+		for enemy in enemies:
+			e_left = enemy[0][0]
+			e_top = enemy[0][1]
+			result[e_top / 32][e_left / 32] = 'E'
+
+		for tile in tiles:
+			t_left = tile[0][0]
+			t_top = tile[0][1]
+			t_type = tile[1]
+			if (t_type == 1 or t_type == 2):
+				result[t_top / 32][t_left / 32] = '@'
+	
+		player_left = player[0][0]
+		player_top = player[0][1]
+
+		result[player_top / 32][player_left / 32] = 'P'
+		
+		self.encoded_map = result
+
+	def print_encoded_map(self):
+		for i in range(13):
+			for j in range(13):
+				sys.stdout.write(self.encoded_map[i][j])
+			sys.stdout.write("\n")
 
 	def Get_mapInfo(self,p_mapinfo):
 		if p_mapinfo.empty()!=True:
