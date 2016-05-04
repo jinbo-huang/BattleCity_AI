27e7c6a1d27856775ad946392c8511cfbd514d13
Jin-Bo Huang <iceman1216i@gmail.com>
2016-05-04 12:33:22 +0800

    The whole logic is completed, but still got some bugs. How to put the tank on multiplier of 32 is current priority. After solving this problem, agent would be a lot better.

--- ai.py
+++ ai.py
@@ -2,6 +2,7 @@ import random
 import time
 import multiprocessing
 import sys
+import Queue
 
 class ai_agent():
 	mapinfo = []
@@ -14,6 +15,10 @@ class ai_agent():
 		self.encoded_map = []
 		self.map_width = 13
 		self.map_height = 13
+		#               up right down left
+		self.dir_top =  [-1, 0,  1,   0]
+		self.dir_left = [0, 1,  0,   -1]
+
 	# rect:					[left, top, width, height]
 	# rect_type:			0:empty 1:brick 2:steel 3:water 4:grass 5:froze
 	# castle_rect:			[12*16, 24*16, 32, 32]
@@ -45,9 +50,14 @@ class ai_agent():
 			player_left = player[0][0]
 			player_top = player[0][1]
 
+
+			q=0
+			for i in range(1000):
+				q+=1
+
 			self.encode_map(bullets, enemies, tiles, player)
 
-			self.print_encoded_map()
+			# self.print_encoded_map()
 			# raw_input()
 			#print "player left:%s" %(player_left)
 			#print "player top:%s" %(player_top)
@@ -59,22 +69,137 @@ class ai_agent():
 			#for i in range(10000000):
 			#	q+=1
 			
+			
+
 			shoot = random.randint(0,1)
 			move_dir = random.randint(0,4)
-			#keep_action = 0
 			keep_action = 1
+
+			# 1. check nearest 6 blocks in every direction ( bullet, tank )
+			# get encoded player position
+			encoded_player_left = player_left / 32
+			encoded_player_top = player_top / 32
+
+			
+			# check for bullets
+			for i in range(4):
+				current_left = encoded_player_left
+				current_top = encoded_player_top
+				for j in range(6):
+					current_left = current_left + self.dir_left[i]
+					current_top = current_top + self.dir_top[i]
+					if (current_left < 0 or current_left >= 13 or current_top < 0 or current_top >= 13):
+						break
+					if (self.encoded_map[current_top][current_left] == 'B'):
+						# print "found bullet"
+						self.Update_Strategy(c_control, 1, i, 1)
+						continue
+
+
+			# check for tanks
+			for i in range(4):
+				current_left = encoded_player_left
+				current_top = encoded_player_top
+				for j in range(6):
+					current_left = current_left + self.dir_left[i]
+					current_top = current_top + self.dir_top[i]
+					if (current_left < 0 or current_left >= 13 or current_top < 0 or current_top >= 13):
+						break
+					if (self.encoded_map[current_top][current_left] == 'E'):
+						# print "found tank"
+						self.Update_Strategy(c_control, 1, i, 1)
+						continue
+
+
+			# 2. check if the position of player's tank is on the multiplier of 32
+
+			# get player's direction
+			player_dir = player[1]
+
+			if ((player_dir == 0 or player_dir == 2) and player_top % 32 != 0):
+				print "adjust top"
+				self.Update_Strategy(c_control, 0, player_dir, keep_action)
+				continue
+			
+			elif ((player_dir == 1 or player_dir == 3) and player_left % 32 != 0):
+				print "adjust left"
+				self.Update_Strategy(c_control, 0, player_dir, keep_action)
+				continue
+
+			#else
+			#	if (
+
+
+			# 3. BFS
+			move = self.bfs()
+			if (move == -1):
+				self.Update_Strategy(c_control, 0, move_dir, keep_action)
+			else:
+				self.Update_Strategy(c_control, 0, move, keep_action)
+			#keep_action = 0
+			
 			#-----------
-			self.Update_Strategy(c_control,shoot,move_dir,keep_action)
+			# self.Update_Strategy(c_control,shoot,move_dir,keep_action)
 		#------------------------------------------------------------------------------------------------------
 
+	def bfs(self):
+		q = Queue.Queue()
+
+		player_left = 0
+		player_top = 0
+
+		for i in range(self.map_height):
+			for j in range(self.map_width):
+				if (self.encoded_map[i][j] == 'P'):
+					player_top = i
+					player_left = j
+					break
+
+		visited = [[False for x in range(self.map_width)] for y in range(self.map_height)]
+		
+		visited[player_top][player_left] = True
+		for i in range(4):
+			new_top = player_top + self.dir_top[i]
+			new_left = player_left + self.dir_left[i]
+			if (new_left < 0 or new_left >= 13 or new_top < 0 or new_top >= 13):
+				continue
+			if (self.encoded_map[new_top][new_left] != '@'):
+				q.put([new_top, new_left, i])
+				visited[new_top][new_left] = True
+
+		result_move = -1
+
+		while not q.empty():
+			temp = q.get()
+			current_top = temp[0]
+			current_left = temp[1]
+			direction = temp[2]
+			visited[current_top][current_left] = True
+
+			if (self.encoded_map[current_top][current_left] == 'E'):
+				print "found enemy"
+				result_move = direction
+				return result_move
+
+			for i in range(4):
+				new_top = current_top + self.dir_top[i]
+				new_left = current_left + self.dir_left[i]
+				if (new_left < 0 or new_left >= 13 or new_top < 0 or new_top >= 13):
+					continue
+				if (visited[new_top][new_left] == False and self.encoded_map[new_top][new_left] != '@'):
+					q.put([new_top, new_left, direction])
+
+		return result_move
 
 	def encode_map(self, bullets, enemies, tiles, player):
 		result = [['_' for x in range(self.map_width)] for y in range(self.map_height)]
 
 		for bullet in bullets:
-			b_left = bullet[0][0]
-			b_top = bullet[0][1]
-			result[b_top / 32][b_left / 32] = 'B'
+			b_left = bullet[0][0] / 32
+			b_top = bullet[0][1] / 32
+			if (b_left < 0 or b_left >= 13 or b_top < 0 or b_top >= 13):
+				continue;
+			result[b_top][b_left] = 'B'
 
 		for enemy in enemies:
 			e_left = enemy[0][0]
